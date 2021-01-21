de3c9ad9f4d36aed0c94ba638f875f41cfe778e7
Abdur Rahman <a.rahman4124@gmail.com>
2021-01-22 03:08:29 +0600

    edit

--- feet.js
+++
@@ -1,9 +0,0 @@
-function kilometerToMeter(num) {
-  let result = num * 1000;
-  if (result < 0) {
-    return "this is not valid number";
-  } else {
-    return result;
-  }
-}
-console.log(kilometerToMeter(10));
